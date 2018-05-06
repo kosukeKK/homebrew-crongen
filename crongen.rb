e2d0bd098bdf417520e122a383957cce115c56ea
kosukeKK <koskacts@gmail.com>
2018-05-06 19:20:51 +0900

    data

--- crongen.rb
+++ crongen.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class CronGen < Formula
+class crongen < Formula
   homepage "https://github.com/kosukeKK/cronGenerator"
   url "https://github.com/mkosukeKK/cronGenerator/release/crongen.tar.gz"
   sha256 "89ce8700a548ed36b242dfe4301195c1293b062i"
